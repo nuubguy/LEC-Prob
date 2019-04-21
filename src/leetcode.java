5e76bcacd91bbcd4dc25af99f59182f0e96a8b45
andries.runkat <andries.runkat@homecredit.co.id>
2019-04-21 22:08:34 +0700

    check linkedlist

--- src/leetcode.java
+++ src/leetcode.java
@@ -985,6 +985,32 @@ public class leetcode {
           return head;
         }
 
+    public ListNode oddEvenList(ListNode head) {
+          if (head == null)
+              return null;
+
+          ListNode odd = head;
+          ListNode even = head.next;
+          ListNode evenHead = even;
+
+
+          while(even!=null && even.next!=null){
+            odd.next = even.next;
+            odd = odd.next;
+            even.next= odd.next;
+            even = even.next;
+          }
+          odd.next = evenHead;
+
+        return odd;
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+    }
+
+
+
 
 
 
